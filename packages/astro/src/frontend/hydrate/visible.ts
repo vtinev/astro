5ec7b6f431176e9f8bcf0540aea33b63a6cdc7ad
Fred K. Schott <fkschott@gmail.com>
2021-08-04 23:25:30 -0700

    do not observe visible hydrate components more than once

--- packages/astro/src/frontend/hydrate/visible.ts
+++ packages/astro/src/frontend/hydrate/visible.ts
@@ -1,11 +1,21 @@
 import type { GetHydrateCallback, HydrateOptions } from '../../@types/hydrate';
 
+// We keep track of all observed `astro-root[uid]` values to make sure that
+// we don't observe the same roots twice. Observing the same root twice
+// causes issues.
+const observedAstroRootIds  = new Set<string>();
+
 /**
  * Hydrate this component when one of it's children becomes visible.
  * We target the children because `astro-root` is set to `display: contents`
  * which doesn't work with IntersectionObserver
  */
 export default async function onVisible(astroId: string, _options: HydrateOptions, getHydrateCallback: GetHydrateCallback) {
+  // Check that we've never observed this element before starting.
+  if (observedAstroRootIds.has(astroId)) {
+    return;
+  }
+  observedAstroRootIds.add(astroId);
   const roots = document.querySelectorAll(`astro-root[uid="${astroId}"]`);
   const innerHTML = roots[0].querySelector(`astro-fragment`)?.innerHTML ?? null;
 
